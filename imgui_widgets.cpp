37c591f4b2c9b8b7b0212878ffa5b817f2f81af0
Jeremi Rynkiewicz <jeremi@jrpc.pl>
2024-03-16 17:08:18 +0100

    restoring original widgets

--- imgui_widgets.cpp
+++ imgui_widgets.cpp
@@ -8793,7 +8793,7 @@ bool    ImGui::TabItemEx(ImGuiTabBar* tab_bar, const char* label, bool* p_open, 
     }
 
     // Click to Select a tab
-    ImGuiButtonFlags button_flags = ImGuiButtonFlags_PressedOnClick | ImGuiButtonFlags_AllowOverlap;
+    ImGuiButtonFlags button_flags = ((is_tab_button ? ImGuiButtonFlags_PressedOnClickRelease : ImGuiButtonFlags_PressedOnClick) | ImGuiButtonFlags_AllowOverlap);
     if (g.DragDropActive && !g.DragDropPayload.IsDataType(IMGUI_PAYLOAD_TYPE_WINDOW)) // FIXME: May be an opt-in property of the payload to disable this
         button_flags |= ImGuiButtonFlags_PressedOnDragDropHold;
     bool hovered, held;
